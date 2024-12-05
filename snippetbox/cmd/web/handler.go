2a23126b8300f2c6f3bbdd84ce1ea5f8b0d5946c
Alex Tao <conquereralex@gmail.com>
2024-12-05 16:52:51 -0500

    add advanced router

--- snippetbox/cmd/web/handler.go
+++ snippetbox/cmd/web/handler.go
@@ -8,14 +8,11 @@ import (
 	"strconv"
 
 	"snippetbox.alextao.net/internal/models"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/" {
-		app.NotFound(w)
-		return
-	}
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -31,7 +28,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.NotFound(w)
 		return
@@ -55,11 +53,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly!\n\n- Kobayashi Issa"
--- snippetbox/go.mod
+++ snippetbox/go.mod
@@ -2,8 +2,10 @@ module snippetbox.alextao.net
 
 go 1.23.2
 
+require github.com/justinas/alice v1.2.0
+
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/justinas/alice v1.2.0
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 )
